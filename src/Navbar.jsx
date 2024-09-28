61ec34d941b6c8434da3ac225c4af8cc4251c671
Brian4755 <brian.mccune1@yahoo.com>
2024-09-28 18:53:05 -0500

    base navbar setup

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return ( 
+    <div className="Navbar">
+      <a href=''>Logo</a>
+      <ul>
+        <li><a href=''>Home</a></li>
+        <li><a href=''>Courses</a></li>
+        <li><a href=''>Contact</a></li>
+        <li><a href=''>About</a></li>
+        <li><a href=''>Sign Up</a></li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default Navbar;
