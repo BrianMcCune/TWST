756da7820eda8ef202922e8cb9fdb27319474a16
Brian4755 <brian.mccune1@yahoo.com>
2024-10-03 11:19:52 -0500

    home screeen base setup

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,24 @@
 const Home = () => {
   return ( 
-    <>
-      This is the Home Page
-    </>
+    <div className="home">
+      <div>
+        <div className="title">Texas Wilderness Survival Training</div>
+        <div className="bio">Build essential survival skills, connect with nature, and learn how to thrive in the wilderness. Prepare for the unexpected with hands-on training.</div>
+      </div>
+      <div className="container"><img src="./fireplace.png" alt="fireplace" /></div>
+      {/* <div className="info">
+        <h2>over 60 hours</h2>
+        <p>Three intense days of immersive survival training.</p>
+      </div>
+      <div className="info">
+        <h2>Professional instructor</h2>
+        <p>Expert-led guidance from experienced survival professionals.</p>
+      </div>
+      <div className="info">
+        <h2>Hands on Learning</h2>
+        <p>Practical, real-world skills in every lesson.</p>
+      </div> */}
+    </div>
    );
 }
  
