0273df61e097dcb1f49a9a3a7b6dc2926d1493b1
Brian4755 <brian.mccune1@yahoo.com>
2024-10-10 00:35:02 -0500

    base text setup for about

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,31 @@
+import './about.css';
+
 const About = () => {
   return ( 
-    <>
-      This is the About Page
-    </>
+    <div className="about">
+      <div>
+        <div className="mission-statement">
+          <h2>Our Mission</h2>
+          <p>
+            At TWST, our mission is to provide families with immersive, hands-on wilderness survival training in the heart of Texas. We emphasize practical learning experiences that equip participants with essential skills for navigating the outdoors. Our courses inspire a deeper appreciation for nature while empowering families to confidently tackle challenges together
+          </p>
+        </div>
+        <div className="image-container">
+
+        </div>
+      </div>
+      <div className="expectations">
+        <div>
+
+        </div>
+        <div>
+          <h2>What to Expect</h2>
+          <p>
+            Join us for an immersive three-day wilderness survival course in [Location], Texas. Under the guidance of our experienced instructor, a retired military professional with [X] years of wilderness survival expertise, you’ll gain practical skills essential for thriving in the outdoors. Expect hands-on training in shelter building, foraging, and navigation, all designed to enhance your confidence and appreciation for nature. Prepare for an unforgettable adventure!
+          </p>
+        </div>
+      </div>
+    </div>
    );
 }
  
