d4b7d1bee6a4c1e78523dfd56c7df89bb1877c11
Brian4755 <brian.mccune1@yahoo.com>
2024-11-18 03:10:56 -0600

    compass base spinning animation

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,9 +1,29 @@
 import './loading.css';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import Compass from './compass';
+
+gsap.registerPlugin(useGSAP);
 
 const Loading = () => {
+
+  useGSAP(
+    () => {
+        gsap.to('.dial', { 
+          rotation:360,
+          // x:360,
+          repeat: -1,
+          duration: 5,
+          transformOrigin:"center center",
+         })
+    }
+)
+
+
   return ( 
     <div className='loading'>
       This is the loading section
+      <Compass />
     </div>
    );
 }
